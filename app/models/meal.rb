1577a8c15aa4164af18ab673117e70abf15a555d
K Smith <ksmith10@chicagobooth.edu>
2014-12-05 17:49:04 -0600

    topdish5

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,4 +3,6 @@ class Meal < ActiveRecord::Base
   belongs_to :dish
   belongs_to :user
 
+  validates :rating, :numericality => true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Routes for the Meal resource:
   # CREATE
   get('/meals/new', { :controller => 'meals', :action => 'new' })
@@ -88,7 +89,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'restaurants#index'
+  root 'meals#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
